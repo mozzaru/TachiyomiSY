849bf02e137819a46f118acb47ba042b8970480b
mozzaru <rianyanuar52@gmail.com>
2025-05-19 21:33:57 +0700

    Version 1.7.5

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,8 +4,8 @@ object AndroidVersions {
     const val compileSdk = 35
     const val minSdk = 23
     const val targetSdk = 34
-    const val versionCode = 111
-    const val versionName = "1.7.4"
+    const val versionCode = 112
+    const val versionName = "1.7.5"
     const val ndk = "23.1.7779620"
     const val kotlin = "2.1.20"
 }
